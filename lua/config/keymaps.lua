cc403c55247319d3cf4c6dea674c9731215c15bb
Tom Clarke <tom@u2i.com>
2025-03-24 16:19:03 -0400

    update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<A-h>", "<C-w>h", { silent = true, desc = "Move to left window" })
+vim.keymap.set("n", "<A-j>", "<C-w>j", { silent = true, desc = "Move to lower window" })
+vim.keymap.set("n", "<A-k>", "<C-w>k", { silent = true, desc = "Move to upper window" })
+vim.keymap.set("n", "<A-l>", "<C-w>l", { silent = true, desc = "Move to right window" })
--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -2,29 +2,96 @@ return {
   {
     "yetone/avante.nvim",
     event = "VeryLazy",
-    dependencies = {
-      "stevearc/dressing.nvim",
-    },
+    version = false,
     opts = {
-      hints = { enabled = false },
+      vendors = {
+        groq = { -- define groq provider
+          __inherited_from = "openai",
+          api_key_name = "GROQ_API_KEY",
+          endpoint = "https://api.groq.com/openai/v1/",
+          model = "llama-3.3-70b-versatile",
+          max_tokens = 32768, -- remember to increase this value, otherwise it will stop generating halfway
+        },
+      },
+      -- hints = { enabled = false },
+      provider = "claude",
+      gemini = {
+        model = "gemini-2.0-pro-exp-02-05",
+      },
+      behaviour = {
+        -- enable_cursor_planning_mode = true, -- enable cursor planning mode!
+      },
+      claude = {
+        timeout = 600000, -- Timeout in milliseconds
+        temperature = 0,
+        max_tokens = 40000,
+      },
+      openai = {
+        endpoint = "https://api.openai.com/v1",
+        model = "o3-mini",
+        timeout = 600000,
+        temperature = 0,
+        max_tokens = 81920,
+        -- reasoning_effort = "high"
+      },
+
+      cursor_applying_provider = "groq",
+      rag_service = {
+        enabled = true, -- Enables the RAG service
+        host_mount = os.getenv("HOME") .. "/dev", -- Host mount path for the rag service
+        provider = "openai", -- The provider to use for RAG service (e.g. openai or ollama)
+        -- llm_model = "", -- The LLM model to use for RAG service
+        -- embed_model = "", -- The embedding model to use for RAG service
+        endpoint = "https://api.openai.com/v1", -- The API endpoint for RAG service
+      },
+      disabled_tools = { "git_commit" },
       -- provider = "claude",
       -- openai = {
       --   model = "o3-mini-2025-01-31",
       --   reasoning_effort = "medium",
       --   timeout = 120000,
       -- },
+    }, -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = "make",
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "echasnovski/mini.pick", -- for file_selector provider mini.pick
+      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+      "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+      "ibhagwan/fzf-lua", -- for file_selector provider fzf
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua", -- for providers='copilot'
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
     },
-    build = LazyVim.is_win() and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" or "make",
-  },
-  {
-    "MeanderingProgrammer/render-markdown.nvim",
-    optional = true,
-    ft = function(_, ft)
-      vim.list_extend(ft, { "Avante" })
-    end,
-    opts = function(_, opts)
-      opts.file_types = vim.list_extend(opts.file_types or {}, { "Avante" })
-    end,
   },
   {
     "folke/which-key.nvim",
